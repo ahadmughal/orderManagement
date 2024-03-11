658cfa11f3cf654cde161863ab94a78ffcb4b5fc
ahadmughal <mr.abdulahad1994@gmail.com>
2024-03-11 17:27:38 +0400

    new module added

--- resources/js/components/admin/Appointments.vue
+++ resources/js/components/admin/Appointments.vue
@@ -1,3 +1,141 @@
+<template>
+        <div class="content-header">
+               <div class="container-fluid">
+                   <div class="row mb-2">
+                       <div class="col-sm-6">
+                           <h1 class="m-0">Appointments Page</h1>
+                       </div>
+                       <div class="col-sm-6">
+                           <ol class="breadcrumb float-sm-right">
+                               <li class="breadcrumb-item"><a href="#">Home</a></li>
+                               <li class="breadcrumb-item active">Appointments</li>
+                           </ol>
+                       </div>
+                   </div>
+               </div>
+           </div>
+
+
+           <div class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="d-flex justify-content-between mb-2">
+                        <div>
+                            <a href="">
+                                <button class="btn btn-primary"><i class="fa fa-plus-circle mr-1"></i> Add New
+                                    Appointment</button>
+                            </a>
+                        </div>
+                        <div class="btn-group">
+                            <button type="button" class="btn btn-secondary">
+                                <span class="mr-1">All</span>
+                                <span class="badge badge-pill badge-info">1</span>
+                            </button>
+
+                            <button type="button" class="btn btn-default">
+                                <span class="mr-1">Scheduled</span>
+                                <span class="badge badge-pill badge-primary">0</span>
+                            </button>
+
+                            <button type="button" class="btn btn-default">
+                                <span class="mr-1">Confirmed</span>
+                                <span class="badge badge-pill badge-success">0</span>
+                            </button>
+
+                            <button type="button" class="btn btn-default">
+                                <span class="mr-1">Cancelled</span>
+                                <span class="badge badge-pill badge-danger">0</span>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="table-responsive">
+                            <table class="table table-bordered">
+                                <thead>
+                                    <tr>
+                                        <th>
+                                        <input
+                                            v-model="selectAll"
+                                            type="checkbox"
+                                            @change="selectAllUsers"
+                                        />
+                                        </th>
+                                        <th scope="col">#</th>
+                                        <th scope="col">Client Name</th>
+                                        <th scope="col">Title</th>
+                                        <th scope="col">Description</th>
+                                        <th scope="col">Start Time</th>
+                                        <th scope="col">Status</th>
+                                        <th scope="col">Options</th>
+                                    </tr>
+                                </thead>
+                                <tbody v-if="appoitments.data.length > 0">
+                                <tr
+                                    v-for="(appointment, index) in appoitments.data"
+                                    :key="appointment.id"
+                                >
+                                    <th>
+                                        <input
+                                            :checked="selectedAll"
+                                            type="checkbox"
+                                            @change="toggleSelection(appointment)"
+                                        />
+                                    </th>
+                                    <th scope="row">{{ index + 1 }}</th>
+                                    <td>{{ appointment.client.first_name }} {{ appointment.client.last_name }}</td>
+                                    <td>{{ appointment.title }}</td>
+                                    <td>{{ appointment.description }}</td>
+                                    <td>{{ moment(appointment.start_time).format("ddd MMM DD, YYYY [at] HH:mm A") }}</td>
+                                    <td>
+                                        <span v-if="appointment.status == 1" class="badge badge-primary">Scheduled</span>
+                                        <span v-if="appointment.status == 2" class="badge badge-success">Confirmed</span>
+                                        <span v-if="appointment.status == 3" class="badge badge-danger">Cancelled</span>
+                                    </td>
+                                    <td>
+                                        <a
+                                            href="#"
+                                            @click.prevent="editUser(appointment)"
+                                            ><i class="fas fa-edit editIcon"></i
+                                        ></a>
+                                        <a
+                                            href="#"
+                                            @click.prevent="
+                                                confirmUserDeletion(appointment)
+                                            "
+                                            ><i class="fas fa-trash delIcon"></i
+                                        ></a>
+                                    </td>
+                                </tr>
+                            </tbody>
+                            <tbody v-else>
+                                <tr>
+                                    <td colspan="7" class="text-center">
+                                        No Record Found......
+                                    </td>
+                                </tr>
+                            </tbody>
+                            </table>
+                        </div>
+                        </div>
+                    </div>
+
+                    <div class="row fullWidth">
+                    <div class="col-12 col-sm-12 col-md-12 col-lg-12 hpadding0">
+                        <Bootstrap4Pagination
+                            :data="appoitments"
+                            @pagination-change-page="getAppoitments"
+                            class="floatRight"
+                        />
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
 <script setup>
 import axios from "axios";
 import * as yup from "yup";
@@ -192,140 +330,3 @@ onMounted(() => {
     getAppoitments();
 });
 </script>
-<template>
-        <div class="content-header">
-               <div class="container-fluid">
-                   <div class="row mb-2">
-                       <div class="col-sm-6">
-                           <h1 class="m-0">Appointments Page</h1>
-                       </div>
-                       <div class="col-sm-6">
-                           <ol class="breadcrumb float-sm-right">
-                               <li class="breadcrumb-item"><a href="#">Home</a></li>
-                               <li class="breadcrumb-item active">Appointments</li>
-                           </ol>
-                       </div>
-                   </div>
-               </div>
-           </div>
-
-
-           <div class="content">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="d-flex justify-content-between mb-2">
-                        <div>
-                            <a href="">
-                                <button class="btn btn-primary"><i class="fa fa-plus-circle mr-1"></i> Add New
-                                    Appointment</button>
-                            </a>
-                        </div>
-                        <div class="btn-group">
-                            <button type="button" class="btn btn-secondary">
-                                <span class="mr-1">All</span>
-                                <span class="badge badge-pill badge-info">1</span>
-                            </button>
-
-                            <button type="button" class="btn btn-default">
-                                <span class="mr-1">Scheduled</span>
-                                <span class="badge badge-pill badge-primary">0</span>
-                            </button>
-
-                            <button type="button" class="btn btn-default">
-                                <span class="mr-1">Confirmed</span>
-                                <span class="badge badge-pill badge-success">0</span>
-                            </button>
-
-                            <button type="button" class="btn btn-default">
-                                <span class="mr-1">Cancelled</span>
-                                <span class="badge badge-pill badge-danger">0</span>
-                            </button>
-                        </div>
-                    </div>
-                    <div class="card">
-                        <div class="card-body">
-                            <div class="table-responsive">
-                            <table class="table table-bordered">
-                                <thead>
-                                    <tr>
-                                        <th>
-                                        <input
-                                            v-model="selectAll"
-                                            type="checkbox"
-                                            @change="selectAllUsers"
-                                        />
-                                        </th>
-                                        <th scope="col">#</th>
-                                        <th scope="col">Client Name</th>
-                                        <th scope="col">Title</th>
-                                        <th scope="col">Description</th>
-                                        <th scope="col">Start Time</th>
-                                        <th scope="col">Status</th>
-                                        <th scope="col">Options</th>
-                                    </tr>
-                                </thead>
-                                <tbody v-if="appoitments.data.length > 0">
-                                <tr
-                                    v-for="(appointment, index) in appoitments.data"
-                                    :key="appointment.id"
-                                >
-                                    <th>
-                                        <input
-                                            :checked="selectedAll"
-                                            type="checkbox"
-                                            @change="toggleSelection(appointment)"
-                                        />
-                                    </th>
-                                    <th scope="row">{{ index + 1 }}</th>
-                                    <td>{{ appointment.client.first_name }} {{ appointment.client.last_name }}</td>
-                                    <td>{{ appointment.title }}</td>
-                                    <td>{{ appointment.description }}</td>
-                                    <td>{{ moment(appointment.start_time).format("ddd MMM DD, YYYY [at] HH:mm A") }}</td>
-                                    <td>
-                                        <span v-if="appointment.status == 1" class="badge badge-primary">Scheduled</span>
-                                        <span v-if="appointment.status == 2" class="badge badge-success">Confirmed</span>
-                                        <span v-if="appointment.status == 3" class="badge badge-danger">Cancelled</span>
-                                    </td>
-                                    <td>
-                                        <a
-                                            href="#"
-                                            @click.prevent="editUser(appointment)"
-                                            ><i class="fas fa-edit editIcon"></i
-                                        ></a>
-                                        <a
-                                            href="#"
-                                            @click.prevent="
-                                                confirmUserDeletion(appointment)
-                                            "
-                                            ><i class="fas fa-trash delIcon"></i
-                                        ></a>
-                                    </td>
-                                </tr>
-                            </tbody>
-                            <tbody v-else>
-                                <tr>
-                                    <td colspan="7" class="text-center">
-                                        No Record Found......
-                                    </td>
-                                </tr>
-                            </tbody>
-                            </table>
-                        </div>
-                        </div>
-                    </div>
-
-                    <div class="row fullWidth">
-                    <div class="col-12 col-sm-12 col-md-12 col-lg-12 hpadding0">
-                        <Bootstrap4Pagination
-                            :data="appoitments"
-                            @pagination-change-page="getAppoitments"
-                            class="floatRight"
-                        />
-                    </div>
-                </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</template>
